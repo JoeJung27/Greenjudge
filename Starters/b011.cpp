b567cdc4151d335b26457d966cf9a44782c81828
鄭渽潤 <joy0974141393@gmail.com>
2020-10-24 15:30:14 +0800

    Update b011.cpp

--- Starters/b011.cpp
+++ Starters/b011.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
+#include<sstream>
+
 using namespace std;
+
 int main(){
+
+	int a,b,c;
 	string l1,l2,l3,ns1,ns2,ns3;
 	cin>>l1>>l2>>l3;
 	char c1[l1.length()],c2[l2.length()],c3[l3.length()];
@@ -23,4 +28,12 @@ int main(){
 			ns3 += c3[i];
 		}
 	}
+
+	stringstream degree(ns1);
+	degree>>a;
+	stringstream degree2(ns2);
+	degree2>>b;
+	stringstream degree3(ns3);
+	degree3>>c;
+	cout<<a+b+c;
 } 
